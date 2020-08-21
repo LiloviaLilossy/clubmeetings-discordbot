bddfc9a87a881f71dc50c6694a49b753ccc8136c
LiloviaLilossy <ddlc.femc@gmail.com>
2020-08-21 09:44:13 +0300

    instamute

--- cogs/cult.py
+++ cogs/cult.py
@@ -1,10 +1,11 @@
 from discord.ext import commands
+from asyncio import sleep
 
 class CultCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.custom_names = {"Baby":"B̶̛̛̰͎͙͚̝̯̩̣͕͆̀̌̊́̍͊̀̈́̐̊̆̀̓͜͠a̸̡̤̥͔̻͛̀b̸̮̤̜̻̺̱̗̓̏̈̓͗̅͒̆̊͌̾͗̉̋̏͜͝͝ẏ̵̧͖͙̟͕̞̫͛͌̏̾̋͛̐͆͗̈́͆̊̇̏͜", "Countess":"Cult Leader", "muted": "Kicked from the Cult", "Charles": "Cult Butler", "Meetings Bot": "Cult Bot"}
-        self.reverse_names = {"B̶̛̛̰͎͙͚̝̯̩̣͕͆̀̌̊́̍͊̀̈́̐̊̆̀̓͜͠a̸̡̤̥͔̻͛̀b̸̮̤̜̻̺̱̗̓̏̈̓͗̅͒̆̊͌̾͗̉̋̏͜͝͝ẏ̵̧͖͙̟͕̞̫͛͌̏̾̋͛̐͆͗̈́͆̊̇̏͜": "Baby", "Cult Leader": "Countess", "Kicked from the Cult": "muted", "Cult Butler": "Charles", "Cult Bot": "Meetings Bot"}
+        self.custom_names = {"Baby":"B̶̛̛̰͎͙͚̝̯̩̣͕͆̀̌̊́̍͊̀̈́̐̊̆̀̓͜͠a̸̡̤̥͔̻͛̀b̸̮̤̜̻̺̱̗̓̏̈̓͗̅͒̆̊͌̾͗̉̋̏͜͝͝ẏ̵̧͖͙̟͕̞̫͛͌̏̾̋͛̐͆͗̈́͆̊̇̏͜", "Countess":"Cult Leader", "muted": "Kicked from the Cult", "Charles": "Cult Butler", "Meetings Bot": "Cult Bot", "Members": "Cultists"}
+        self.reverse_names = {"B̶̛̛̰͎͙͚̝̯̩̣͕͆̀̌̊́̍͊̀̈́̐̊̆̀̓͜͠a̸̡̤̥͔̻͛̀b̸̮̤̜̻̺̱̗̓̏̈̓͗̅͒̆̊͌̾͗̉̋̏͜͝͝ẏ̵̧͖͙̟͕̞̫͛͌̏̾̋͛̐͆͗̈́͆̊̇̏͜": "Baby", "Cult Leader": "Countess", "Kicked from the Cult": "muted", "Cult Butler": "Charles", "Cult Bot": "Meetings Bot", "Cultists": "Members"}
     
     @commands.is_owner()
     @commands.group(invoke_without_command=False)
@@ -15,6 +16,7 @@ class CultCog(commands.Cog):
     async def cult_start(self, ctx):
         cm = self.bot.get_guild(730055007226953768)
         for role in cm.roles:
+            sleep(2)
             try:
                 if role.name in ["Blue Cat", "Groovy", "Tupperbox"]: continue
                 if role.name in list(self.custom_names):
@@ -24,11 +26,13 @@ class CultCog(commands.Cog):
                 await role.edit(name=name)
             except: continue
         for channel in cm.text_channels:
+            sleep(2)
             try:
                 name = "cult-"+channel.name
                 await channel.edit(name=name)
             except: continue
         for channel in cm.voice_channels:
+            sleep(2)
             try:
                 name = "Cult "+channel.name
                 await channel.edit(name=name)
@@ -42,6 +46,7 @@ class CultCog(commands.Cog):
     async def cult_end(self, ctx):
         cm = self.bot.get_guild(730055007226953768)
         for role in cm.roles:
+            sleep(2)
             try:
                 if role.name in ["Blue Cat", "Groovy", "Tupperbox"]: continue
                 if role.name in list(self.reverse_names):
@@ -51,11 +56,13 @@ class CultCog(commands.Cog):
                 await role.edit(name=name)
             except: continue
         for channel in cm.text_channels:
+            sleep(2)
             try:
                 name = channel.name[5:]
                 await channel.edit(name=name)
             except: continue
         for channel in cm.voice_channels:
+            sleep(2)
             try:
                 name = channel.name[5:]
                 await channel.edit(name=name)
--- cogs/role.py
+++ cogs/role.py
@@ -1,14 +1,32 @@
 from discord.ext import commands
 from discord import Role
+from json import load, dump
 
 class RoleGiverCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.instamute_cache = load(open("bot-settings/instamute.json", "r"))["muted"] or []
     
     @commands.command()
     async def verify(self, ctx):
         role = ctx.guild.get_role(730055612037333062)
         await ctx.author.add_roles(role)
+    
+    @commands.is_owner()
+    @command.command()
+    async def instamute(self, ctx, id):
+        self.instamute_cache.append(id)
+        data = load(open("bot-settings/instamute.json", "r"))
+        data.append(id)
+        dump(data, open("bot-settings/instamute.json", "w"))
+        await ctx.send("User was instamuted, if they'll join, I'll add a muted role.")
+    
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        if member.id in self.instamute_cache:
+            role = member.guild.get_role(738982557999169627)
+            await member.add_roles(role, reason="Is in instamute list.")
+            await member.send("You were muted from Doki Doki Club Meetings Discord Server. It's better to ask LiloviaLilossy#4389 for reason.")
 
 def setup(bot):
     bot.add_cog(RoleGiverCog(bot))
