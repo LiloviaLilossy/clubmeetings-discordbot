1b3e15aa25b8b8771b7b5f124eba52a31359cbd0
LiloviaLilossy <ddlc.femc@gmail.com>
2020-08-24 07:38:21 +0300

    leaderboard v1

--- cogs/counter.py
+++ cogs/counter.py
@@ -1,5 +1,7 @@
+from discord import Embed, Color
 from discord.ext import commands
 from json import load, dump
+import datetime
 
 class Counter(commands.Cog):
     def __init__(self, bot):
@@ -24,6 +26,27 @@ class Counter(commands.Cog):
                 member = msglist.index((id, count))
                 break
         await ctx.send(f"Messages: {file[str(ctx.author.id)]}, #{member+1} on leaderboard")
+    
+    @commands.command()
+    async def leaderboard(self, ctx):
+        roles = [740608292979474443, 730076720308420681, 730055873069711440, 734050572088508448, 740564108377718846, 744545560295374858, 744559294959059065]
+        e = Embed(color=Color.gold)
+        e.description = "Club Meetings Leaderboard!"
+        e.timestamp = datetime.datetime.utcnow()
+        text = ""
+        file = load(open("bot-settings/counter.json", "r"))
+        msglist = list(file.items())
+        msglist.sort(key=lambda i: i[1], reverse=True)
+        for id, count in msglist:
+            breaking = False
+            member = ctx.guild.get_member(id)
+            for role in member.roles:
+                if role.id in roles:
+                    breaking = True
+            if breaking: continue
+            text += f"#{msglist.index((id, count))+1} - {member.name} ({count} messages) \n"
+        e.add_field(name="There are only members without custom roles.", value=text)
+        await ctx.send(embed=e)
 
 def setup(bot):
     bot.add_cog(Counter(bot))
